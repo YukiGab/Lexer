410d08060e8f66c3b33427da49b57c970e162f0b
YukiGab <yukio.gabriel.h@gmail.com>
2026-03-15 00:37:18 -0600

    Avance

--- mx/unam/fi/compilers/g5/05/src/lector.py
+++ mx/unam/fi/compilers/g5/05/src/lector.py
@@ -4,32 +4,76 @@ from lexertable import token
 def analizearchive(ruta):
     tokens = []
     position = 0
-    with open(ruta,'r') as archivo:
-        contenido = archivo.read().splitlines()            
-    while position < len(contenido):
-        palabra = contenido[i]
-        palabraval = False
+    try:
+        with open(ruta,'r') as archivo:
+            contenido = archivo.read().splitlines()     
 
-        for palabras, tokey in token:
-            validar = re.compile(palabras)
-            comparar = validar.match(palabra)
 
-            if comparar:
-                valor = match.group(0)
-                if tokey:
-                    tokens.append(tokey)
-            
-            position += len(valor)
-            palabraval = True
-            break
-    
-    if not palabraval:
-        print(f"Error: Invalid symbol or symbol not found in the word: '{palabra[0]}'")
-        break
+        for linea in contenido:
+            position = 0
+
+            while position < len(contenido):
+                
+                palabra = contenido[posicion]
+                posicion += 1
+                palabraval = False
+
+                for palabras, tokens in token:
+                    validar = re.compile(palabras)
+                    comparar = validar.match(palabra)
+
+                    if comparar:
+                        valor = comparar.group(0)
+                        if tokens:
+                            tokens.append(tokens)
+                    
+                    position += len(valor)
+                    palabraval = True
+
+                    break
+        
+                if not palabraval:
+                    print(f"Error: Invalid symbol at '{linea[position]}'")
+                    return
+
+        print(" ".join(tokens))
+        print(f"Total of tokens: {len(tokens)}")
+    except FileNotFoundError:
+        print(f"Error, file not found in {ruta}")
+
+def analizeterminal(code):
+    tokens =[]
+    coded = code.splitlines() 
+    for linea in coded:
+            position = 0
+
+
+
+            while position < len(coded):
+                if linea[position].isspace():
+                    position += 1
+                    continue
+
+                palabra = coded[position]
+                palabraval = False
+
+                for palabras, tokens in token:
+                    validar = re.compile(palabras)
+                    comparar = validar.match(palabra)
+
+                    if comparar:
+                        valor = comparar.group(0)
+                        if tokens:
+                            tokens.append(tokens)
+                    
+                        position += len(valor)
+                        palabraval = True
+
+                    break
+        
+            if not palabraval:
+                print(f"Error: Invalid symbol at '{linea[position]}'")
+                return
 
     print(" ".join(tokens))
     print(f"Total of tokens: {len(tokens)}")
-
-def analizeterminal(code):
-    print(code)    
-
--- mx/unam/fi/compilers/g5/05/src/main.py
+++ mx/unam/fi/compilers/g5/05/src/main.py
@@ -4,13 +4,19 @@ def main():
         seleccion = input("Select how you will enter your code (archive/terminal): ").strip().lower()
         match seleccion:
             case "archive":
-                ruta = input("Enter your file path:\n")
-                lector.analizearchive(ruta)
+                try:
+                    ruta = input("Enter your file path:\n")
+                    lector.analizearchive(ruta)
+                except:
+                    print("Erron, invalid location")
+                
                 break
             case "terminal":
                 code = input("Enter your code:\n")
                 lector.analizeterminal(code)
                 break
+            case "exit":
+                exit
             case _:
                 print("Error: Invalid option. Please try again.\n")
 
